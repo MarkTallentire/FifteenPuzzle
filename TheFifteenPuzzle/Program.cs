8803ea05c0f8f25eddf57aca183e70b91950361e
Mark Tallentire <mark.tallentire@hazcheck.com>
2024-02-19 19:53:43 +0000

    complete

--- TheFifteenPuzzle/Program.cs
+++ TheFifteenPuzzle/Program.cs
@@ -1,29 +1,144 @@
 ﻿var board = new Board();
+var input = new InputController();
+
+
+while (true)
+{
+    board.ProcessMove(input.GetInput());
+    board.Update();
+}
+
+
+public class InputController()
+{
+    public Direction GetInput()
+    {
+        var key = Console.ReadKey(false).Key;
+
+        return key switch
+        {
+            ConsoleKey.W => Direction.Up,
+            ConsoleKey.S => Direction.Down,
+            ConsoleKey.A => Direction.Left,
+            ConsoleKey.D => Direction.Right,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+}
 
 public class Board
 {
-    private const int BOARD_WIDTH = 4; //x
-    private const int BOARD_HEIGHT = 4; //y
+    private const int BoardWidth = 4; //x
+    private const int BoardHeight = 4; //y
+    
+    private int _blankPosX = 0;
+    private int _blankPosY = 0;
 
-    private int? [,] _spaces;
+    private readonly int? [,] _spaces;
 
     public Board()
     {
-        _spaces = new int?[BOARD_WIDTH, BOARD_HEIGHT];
-        FillSpaces();
+        _spaces = new int?[BoardWidth, BoardHeight];
         PickRandomStartPosition();
-        Render();
+        FillSpaces();
+        Update();
     }
 
+    public void ProcessMove(Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.Down:
+            {
+                if (_blankPosY + 1 >= BoardHeight) break;
+                
+                var swap = _spaces[_blankPosX, _blankPosY + 1];
+                _spaces[_blankPosX, _blankPosY + 1] = null;
+                _spaces[_blankPosX, _blankPosY] = swap;
+
+                _blankPosY += 1;
+                break;
+            }
+            case Direction.Up:
+            {
+                if (_blankPosY - 1 < 0) break;
+                
+                var swap = _spaces[_blankPosX, _blankPosY - 1];
+                _spaces[_blankPosX, _blankPosY - 1] = null;
+                _spaces[_blankPosX, _blankPosY] = swap;
+
+                _blankPosY -= 1;
+                break;
+            }
+            case Direction.Right:
+            {
+                if (_blankPosX + 1 >= BoardWidth) break;
+                
+                var swap = _spaces[_blankPosX + 1, _blankPosY];
+                _spaces[_blankPosX + 1, _blankPosY ] = null;
+                _spaces[_blankPosX, _blankPosY] = swap;
+
+                _blankPosX += 1;
+                break;
+            }
+            case Direction.Left:
+            {
+                if (_blankPosX - 1 < 0) break;
+                
+                var swap = _spaces[_blankPosX - 1, _blankPosY];
+                _spaces[_blankPosX - 1, _blankPosY] = null;
+                _spaces[_blankPosX, _blankPosY] = swap;
+
+                _blankPosX -= 1;
+                break;
+            }
+        }
+        Console.WriteLine(direction);
+    }
+    public void Update()
+    {
+        Console.Clear();
+        Render();
+
+        if (HasWon())
+        {
+            Console.WriteLine("You're winner");
+            Environment.Exit(0);
+        };
+    }
+
+    private bool HasWon()
+    {
+        var flattened = new int? [BoardWidth * BoardHeight];
+        var flatTrack = 0;
+        //flatten the 2d array into one array.
+        for (var y = 0; y < BoardHeight; y++)
+        {
+            for (var x = 0; x < BoardWidth; x++)
+            {
+                flattened[flatTrack] = _spaces[x, y];
+                flatTrack++;
+            }
+        }
+
+        for (int i = 1; i < flattened.Length; i++)
+        {
+            if (flattened[i - 1] != flattened[i] + 1)
+                return false;
+        }
+
+        return true;
+    }
+    
     private void Render()
     {
         for (int y = 0; y < _spaces.GetLength(0); y++)
         {
             for (int x = 0; x < _spaces.GetLength(1); x++)
             {
-                if (x != BOARD_WIDTH - 1)
+                if (x != BoardWidth - 1)
                 {
-                    Console.Write(_spaces[x, y] is null ? ", " : $"{_spaces[x, y]}, ");
+                    Console.Write(_spaces[x, y] is null ? " \u2588, " : $" {_spaces[x, y]}, ");
                 }
                 else
                 {
@@ -33,13 +148,15 @@ public class Board
             Console.WriteLine();
         }
     }
-    
     private void PickRandomStartPosition()
     {
         var random = new Random();
-        var x = random.Next(0, BOARD_WIDTH + 1);
-        var y = random.Next(0, BOARD_HEIGHT + 1);
+        var x = random.Next(0, BoardWidth);
+        var y = random.Next(0, BoardHeight);
 
+        _blankPosX = x;
+        _blankPosY = y;
+        
         _spaces[x,y] = null;
     }
     private void FillSpaces()
@@ -50,10 +167,10 @@ public class Board
         {
             for (int x = 0; x < _spaces.GetLength(1); x++)
             {
-                int? number = null;
+                int? number = _spaces[x,y];
                 while (number is null || IsNumberUsed(number))
                 {
-                    number = random.Next(1, BOARD_HEIGHT * BOARD_WIDTH + 1);
+                    number = random.Next(1, BoardHeight * BoardWidth + 1);
                 }
                 _spaces[x, y] = Convert.ToInt32(number);
             }
@@ -74,3 +191,5 @@ public class Board
         }
     }
 }
+
+public enum Direction {Up, Down, Left, Right};
--- TheFifteenPuzzle/obj/Debug/net8.0/TheFifteenPuzzle.AssemblyInfo.cs
+++ TheFifteenPuzzle/obj/Debug/net8.0/TheFifteenPuzzle.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TheFifteenPuzzle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7194ce7a922e089fee2ba349dfed2a2b3ba05a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TheFifteenPuzzle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TheFifteenPuzzle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
