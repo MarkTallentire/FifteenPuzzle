bd42eb28adf35221d1dd4d0a6c5bf1c49552cc8d
Mark Tallentire <mark.tallentire@hazcheck.com>
2024-02-19 16:53:21 +0000

    With Can Slide

---
+++ TheFifteenPuzzle/Program.cs
@@ -0,0 +1,104 @@
+﻿var board = new Board();
+
+public class Board
+{
+    private const int BOARD_WIDTH = 4; //x
+    private const int BOARD_HEIGHT = 4; //y
+
+    private int? [,] _spaces;
+
+    public Board()
+    {
+        _spaces = new int?[BOARD_WIDTH, BOARD_HEIGHT];
+        FillSpaces();
+        PickRandomStartPosition();
+        Render();
+    }
+
+    private void Render()
+    {
+        for (int y = 0; y < _spaces.GetLength(0); y++)
+        {
+            for (int x = 0; x < _spaces.GetLength(1); x++)
+            {
+                if (CanSlide(x, y))
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                else
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                
+                
+                if (x != BOARD_WIDTH - 1)
+                {
+                    Console.Write(_spaces[x, y] is null ? ", " : $"{_spaces[x, y]}, ");
+                }
+                else
+                {
+                    Console.Write(_spaces[x, y] is null ? "" : $"{_spaces[x, y]}");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+    
+    private void PickRandomStartPosition()
+    {
+        var random = new Random();
+        var x = random.Next(0, BOARD_WIDTH + 1);
+        var y = random.Next(0, BOARD_HEIGHT + 1);
+
+        _spaces[x,y] = null;
+    }
+
+    bool CanSlide(int indexX, int indexY)
+    {
+        //If we are a direct neighbour of the blank we can slide.
+        for (int y = 0; y < _spaces.GetLength(0); y++)
+        {
+            for (int x = 0; x < _spaces.GetLength(1); x++)
+            {
+                if (_spaces[x, y] == null)
+                {
+                    if (indexX == x + 1 && indexY == y) return true;
+                    if (indexX == x - 1 && indexY == y) return true;
+                    if (indexY == y + 1 && indexX == x) return true;
+                    if (indexY == y - 1 && indexX == x) return true;
+                }
+                    
+            }
+        }
+
+        return false;
+    }
+
+    private void FillSpaces()
+    {
+        var random = new Random();
+
+        for (int y = 0; y < _spaces.GetLength(0); y++)
+        {
+            for (int x = 0; x < _spaces.GetLength(1); x++)
+            {
+                int? number = null;
+                while (number is null || IsNumberUsed(number))
+                {
+                    number = random.Next(1, BOARD_HEIGHT * BOARD_WIDTH + 1);
+                }
+                _spaces[x, y] = Convert.ToInt32(number);
+            }
+        }
+
+        bool IsNumberUsed(int? number)
+        {
+            for (int y = 0; y < _spaces.GetLength(0); y++)
+            {
+                for (int x = 0; x < _spaces.GetLength(1); x++)
+                {
+                    if (_spaces[x, y] == number)
+                        return true;
+                }
+            }
+            
+            return false;
+        }
+    }
+}
